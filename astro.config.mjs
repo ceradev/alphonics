5a87781196b91d51e71bd7252055a4c8ed806a54
César Suárez <suarezorizondocesararamis@gmail.com>
2024-03-05 13:42:00 +0000

    Añadir archivos ignorados y actualizar README.md

---
+++ astro.config.mjs
@@ -0,0 +1,64 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import AstroPWA from '@vite-pwa/astro'
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    logLevel: 'info',
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
+  },
+  integrations: [
+    react(), 
+    tailwind(),
+    
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Alphonic PWA',
+        short_name: 'Alphonic',
+        theme_color: '#b81414',
+        display: 'standalone',
+        background_color: '#666666',
+        description: 'Alphonics es más que una aplicación de música; es tu espacio personal para explorar, descubrir y crear sonidos únicos.',
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    }),
+  ],
+});
